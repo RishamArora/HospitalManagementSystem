29e65c0f8230f5083acff68b4e183e483d518388
RishamArora <69429468+RishamArora@users.noreply.github.com>
2024-04-17 23:29:52 +0530

    Added Functionalities

--- src/main/java/com/example/HospitalManagementSystem/doclogin/controller/AppointmentsController.java
+++ src/main/java/com/example/HospitalManagementSystem/doclogin/controller/AppointmentsController.java
@@ -1,8 +1,16 @@
 package com.example.HospitalManagementSystem.doclogin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.HospitalManagementSystem.doclogin.entity.Appointments;
 import com.example.HospitalManagementSystem.doclogin.repository.AppointmentsRepository;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v2")
 public class AppointmentsController {
@@ -31,6 +40,16 @@ public class AppointmentsController {
 	@GetMapping
 	public List<Appointments> getAllAppointments(){
 		return appointmentsRepository.findAll();
+	}
+	
+	@DeleteMapping("/appointments/{id}")
+	public ResponseEntity<Map<String,Boolean>> deleteAppointment(@PathVariable long id) throws AttributeNotFoundException{
+	Appointments appointments=	appointmentsRepository.findById(id).orElseThrow(()->new AttributeNotFoundException("Appointment Not Found With This ID"));
+	appointmentsRepository.delete(appointments);
+	Map<String, Boolean> response = new HashMap<String,Boolean>();
+	response.put("Deleted", Boolean.TRUE);
+	
+	return ResponseEntity.ok(response);
 	}
 
 }
